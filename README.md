01e72e072ba940cc98c764fb86b6d17d0a983a66
mishra <tech@golivelabs.io>
2015-07-08 12:10:18 -0700

    jsdoc

---
+++ README.md
@@ -0,0 +1,3 @@
+#Tasks
+
+ - Explore (jsdocs)[http://usejsdoc.org/]
---
+++ ajax-call.js
@@ -0,0 +1,24 @@
+/**
+ * @module MyModule
+ * @description Ajax Utility Functions
+ */
+
+/**
+ * @function makeAjaxCall
+ * @description This will make an ajax call
+ * @param url The url that you want to access
+ * @param callback The callback or function that will be executed once the call is complete
+ */
+function makeAjaxCall(url, callback) {
+    var xhr = new XMLHttpRequest;
+    console.log(xhr.readyState);
+    xhr.addEventListener('readystatechange', function () {
+        console.log(xhr.readyState);
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var obj = JSON.parse(xhr.responseText);
+            callback(obj);
+        }
+    });
+    xhr.open('get', url);
+    xhr.send();
+}
